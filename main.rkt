08123481274b4bf4859c94eb0fc495954bd13991
Geoffrey Knauth <geoff@knauth.org>
2019-06-19 00:22:15 -0400

    add functions to support WBGT

--- main.rkt
+++ main.rkt
@@ -22,7 +22,7 @@
   
 (struct raw-obs (ceil cover dt dewpoint precip? wx day? rain-in prob-rain realfeel rh temp uv uvtext viz wxicon windir wintxt windspeed gust) #:transparent)
   
-(struct obs (ceil cover dt dewpoint precip? wx day? rain-in prob-rain realfeel rh temp uv uvtext viz wxicon windir wintxt windspeed gust) #:transparent)
+(struct obs (raw ceil cover dt dewpoint precip? wx day? rain-in prob-rain realfeel rh temp uv uvtext viz wxicon windir wintxt windspeed gust) #:transparent)
 
 (define const-hPa 1012.0)
 
@@ -32,10 +32,16 @@
      (* 0.1 tempc-air)))
 
 (define (meters-per-hour miles-per-hour)
-  (* 1609.34 meters-per-hour))
+  (* 1609.34 miles-per-hour))
 
 (define const-sigma 5.67e-8)
 (define const-h 0.315)
+
+(define (f2c tempf)
+  (* (/ 5.0 9.0) (- tempf 32.0)))
+
+(define (c2f tempc)
+  (+ 32 (* tempc (/ 9.0 5.0))))
 
 (define (atmospheric-vapor-pressure dewpointc ambient-tempc hPa)
   (* (exp (/ (* 17.67 (- dewpointc ambient-tempc))
@@ -48,9 +54,9 @@
 (define (thermal-emmissivity dewpointc ambient-tempc hPa)
   (* 0.575 (expt (atmospheric-vapor-pressure dewpointc ambient-tempc hPa) 1/7)))
 
-(define (wbgt-B solar-watts-per-meter-squared
-                direct-beam-radiation-from-sun diffuse-radiation-from-sun zenith-r
-                dewpointc ambient-tempc hPa)
+(define (wbgt-part-B solar-watts-per-meter-squared
+                     direct-beam-radiation-from-sun diffuse-radiation-from-sun zenith-r
+                     dewpointc ambient-tempc hPa)
   (+ (* solar-watts-per-meter-squared
         (+ (/ direct-beam-radiation-from-sun
               (* 4 const-sigma (cos zenith-r)))
@@ -58,18 +64,34 @@
      (* (thermal-emmissivity dewpointc ambient-tempc hPa)
         (expt ambient-tempc 4))))
 
-(define (wbgt-C wind-meters-per-hour)
+(define (wbgt-part-C wind-meters-per-hour)
   (/ (* const-h (expt wind-meters-per-hour 0.58)) 5.53865e-8))
 
-(define (tempc-globe wind-meters-per-hour ambient-tempc dewpointc solar-watts-per-meter-squared
-                     direct-beam-radiation-from-sun diffuse-radiation-from-sun zenith-r hPa)
-  (let ([C (wbgt-C wind-meters-per-hour)])
-    (/ (+ (wbgt-B solar-watts-per-meter-squared
-                  direct-beam-radiation-from-sun diffuse-radiation-from-sun zenith-r
-                  dewpointc ambient-tempc hPa)
+(define (wbgt-C wind-meters-per-hour ambient-tempc dewpointc solar-watts-per-meter-squared
+                direct-beam-radiation-from-sun diffuse-radiation-from-sun zenith-r hPa)
+  (let ([C (wbgt-part-C wind-meters-per-hour)])
+    (/ (+ (wbgt-part-B solar-watts-per-meter-squared
+                       direct-beam-radiation-from-sun diffuse-radiation-from-sun zenith-r
+                       dewpointc ambient-tempc hPa)
           (* C ambient-tempc)
           7680000)
        (+ C 256000))))
+
+(define (wbgt-fahrenheit o)
+  (c2f (wbgt-celsius o)))
+
+(define (wbgt-celsius o)
+  (let ([r (obs-raw o)])
+    (wbgt-C
+     (meters-per-hour (raw-obs-windspeed r))
+     (f2c (raw-obs-temp r))
+     (f2c (raw-obs-dewpoint r))
+     (est-irradiance (obs-cover o) (raw-obs-day? r))
+     (radiation-from-sun 'direct-beam 'summer)
+     (radiation-from-sun 'diffuse 'summer)
+     zenith-radians
+     1011.85)))
+  
   
 (define (sky cover)
   (let ([eighths (/ cover (/ 100 8.))])
@@ -78,6 +100,25 @@
           [(<= eighths 4) "SCT"]
           [(<= eighths 7) "BKN"]
           [else "OVC"])))
+
+(define (est-irradiance sky-cover day?)
+  (if day?
+      0
+      (cond [(string=? sky-cover "SKC") 990]
+            [(string=? sky-cover "FEW") 980]
+            [(string=? sky-cover "SCT") 980]
+            [(string=? sky-cover "BKN") 710]
+            [(string=? sky-cover "OVC") 250]
+            [else 0])))
+
+(define (radiation-from-sun kind season)
+  (cond [(eq? kind 'direct-beam) (cond [(eq? season 'summer) 0.75]
+                                       [(eq? season 'winter) 0.67]
+                                       [else 0.71])]
+        [(eq? kind 'diffuse) (cond [(eq? season 'summer) 0.25]
+                                   [(eq? season 'winter) 0.33]
+                                   [else 0.29])]
+        [else 0.5]))
   
 (define (fmt-i-03d n)
   (let ((s (format "~a" n)))
@@ -89,7 +130,7 @@
   (fmt-i-03d (exact-round (/ ft 100.))))
 
 (define (heat-category-class tempf)
-  (let ([x (first (heat-category (string->number (substring tempf 2 4))))])
+  (let ([x (heat-category tempf)])
     (cond [(= x 2) "heatgreen"]
           [(= x 3) "heatyellow"]
           [(= x 4) "heatred"]
@@ -97,12 +138,12 @@
           [else "heatwhite"])))
 
 (define (heat-category wbgt)
-  (cond [(and (>= wbgt 78.0) (< wbgt 82.0)) (list 1 #f)]
-        [(and (>= wbgt 82.0) (< wbgt 85.0)) (list 2 "green")]
-        [(and (>= wbgt 85.0) (< wbgt 88.0)) (list 3 "yellow")]
-        [(and (>= wbgt 88.0) (< wbgt 90.0)) (list 4 "red")]
-        [(>= wbgt 90.0) (list 5 "black")]
-        [else (list 0 #f)]))
+  (cond [(and (>= wbgt 78.0) (< wbgt 82.0)) 1]
+        [(and (>= wbgt 82.0) (< wbgt 85.0)) 2]
+        [(and (>= wbgt 85.0) (< wbgt 88.0)) 3]
+        [(and (>= wbgt 88.0) (< wbgt 90.0)) 4]
+        [(>= wbgt 90.0) 5]
+        [else 0]))
   
 (define days-of-week (vector "Sun" "Mon" "Tue" "Wed" "Thu" "Fri" "Sat"))
   
@@ -134,6 +175,7 @@
 
 (define (raw->obs r)
   (obs
+   r
    (feet-hundreds (raw-obs-ceil r))
    (sky (raw-obs-cover r))
    (let ([x (raw-obs-dt r)])
@@ -164,13 +206,15 @@
 (define rxi-showers #rx"(?i:showers)")
 (define rxi-thunderstorms #rx"(?i:thunderstorms)")
 (define rxi-t-storms #rx"(?i:t-storms)")
+(define rx-realfeel #rx"(\\d+).*")
 
 (define align-right "ra")
 
 (define (obs->tr o)
   `(tr
     ,(td-class-s (if (obs-day? o) "day" "night") (obs-dt o))
-    (td "") ;,(td-class-s (heat-category-class (obs-realfeel o)) "")
+    (td "")
+    ;,(td-class-s (heat-category-class (wbgt-fahrenheit o)) (number->string(wbgt-fahrenheit o)))
     ,(let ([x (obs-wx o)])
        (cond [(regexp-match rxi-showers x)
               (td-class-s "green" x)]
